de39ea6b36609ffc0084f231dbd00661282d4fcc
Andrey Isambaev <isandrey2008@yandex.ru>
2025-04-09 03:35:49 +0400

    final v01

--- main.py
+++ main.py
@@ -6,23 +6,24 @@ from bs4 import BeautifulSoup
 from datetime import datetime as dt
 
 
-KEYWORDS = ['data scrap', 'postgres', 'web', 'python']
-KEYWORDS = ['data scrap']
+KEYWORDS = ['фото', 'дизайн', 'web', 'python']
 WEB_PORTAL = r'https://habr.com'
 MAX_PAGES = 50
-DOWNLOAD_FOLDER = 'pages'
 RE_COMPILE = re.compile('|'.join(KEYWORDS), re.IGNORECASE)
-SEMAPHORE_10 = Semaphore(10)
+SEMAPHORE_10 = Semaphore(10)  # Запустим одновременно 10 потоков для скраппинга
+count_articles = 0
 
 
 def get_endpoint(page_number=1):
+    """Получает эндпоинт по номеру страницы."""
     if page_number == 1 or page_number > MAX_PAGES:
-        return r'https://habr.com/ru/articles/page01'
+        return r'https://habr.com/ru/articles/'
     endpoint = r'/ru/articles/page' + '{:02}'.format(page_number) + r'/'
     return WEB_PORTAL + endpoint
 
 
 def article_is_found(preview, full_text=''):
+    """Проверяет наличие в тексте ключевых слов."""
     if len(KEYWORDS) == 0:
         return True
     text_to_search = preview
@@ -32,23 +33,26 @@ def article_is_found(preview, full_text=''):
 
 
 def extract_text(tag):
+    """Получает текст из тэга."""
     if tag.sting is not None:
-        return str(tag.string)
+        return tag.string
     result = ''
     for s in tag.stripped_strings:
         result += s
     return result
 
+
 def get_web_page(url):
+    """Получает текст вэб страницы."""
     headers = FakeHttpHeader().as_header_dict()
-    response = requests.get(url=WEB_PORTAL, headers=headers)
-    with open('headers.txt', 'w', encoding='utf-8') as h:
-        h.write(str(headers))
+    response = requests.get(url=url, headers=headers)
     if response.status_code == 200:
         return response.text
     return None
 
+
 def get_formatted_body(tag):
+    """Поиск тэга со статьей."""
     return (
         tag.find(
             name='div',
@@ -60,24 +64,22 @@ def get_formatted_body(tag):
         )
     )
 
-'''
-with open('habr.html', 'r', encoding='utf-8') as f:
-    soup = BeautifulSoup(f, 'lxml')
-    with open('soup.html', 'w', encoding='utf-8') as s:
-        s.write(soup.prettify())
-'''
 
 def main(page_number=1):
-    # SEMAPHORE_10.acquire()
-    print(get_endpoint(page_number))
+    """Скраппинг."""
+    SEMAPHORE_10.acquire()
+
+    global count_articles
     web_page = get_web_page(get_endpoint(page_number))
     if web_page is None:
         return
-    with open('habr.html', 'w', encoding='utf-8') as f:
-        f.write(web_page)
     html = BeautifulSoup(web_page, 'lxml')
+    if html is None:
+        return
     articles = html.find_all(name='article', class_='tm-articles-list__item')
-    print(len(articles))
+    if articles is None:
+        return
+
     for article in articles:
         # Время публикации
         article_time = article.find(name='time')
@@ -89,41 +91,40 @@ def main(page_number=1):
         if article_title is None:
             continue
         href = WEB_PORTAL+article_title['href']
-        title = article_title.span.string
-        # Текст превью используются 2 формата
-        article_preview_v1, article_preview_v2 = get_formatted_body(article)
-
+        title = extract_text(article_title.span)
+        # Текст превью
         preview = ''
+        article_preview_v1, article_preview_v2 = get_formatted_body(article)     
         if article_preview_v1 is not None:
             preview = extract_text(article_preview_v1)
         elif article_preview_v2 is not None:
             preview = extract_text(article_preview_v2)
-
+        # Полный текст статьи
         body = ''
         article_source = get_web_page(href)
         if article_source is not None:
             html = BeautifulSoup(article_source, 'lxml')
             if html is not None:
-                article_preview_v1, article_preview_v2 = get_formatted_body(html)
-                if article_preview_v1 is not None:
-                    body = extract_text(article_preview_v1)
-                elif article_preview_v2 is not None:
-                    body = extract_text(article_preview_v2)
+                article_body_v1, article_body_v2 = get_formatted_body(html)
+                if article_body_v1 is not None:
+                    body = extract_text(article_body_v1)
+                elif article_body_v2 is not None:
+                    body = extract_text(article_body_v2)
 
         if article_is_found(preview, body):
-            print(date, title, href, sep=' - ')
-        else:
-            print(date, title, href, sep=' / ')
-    # SEMAPHORE_10.release()
+            # Дополнительно к ТЗ выводится номер страницы, на которой найдена статья
+            print(date, title, href, f'страница {page_number}', sep=' - ')
+            count_articles += 1
+
+    SEMAPHORE_10.release()
 
 
 if __name__=='__main__':
-    '''
-    threads = [Thread(target=main, args=(i,)) for i in range(1, 2)]
+    threads = [Thread(target=main, args=(i,)) for i in range(1, MAX_PAGES+1)]
     for t in threads:
         t.start()
 
     for t in threads:
         t.join()
-    '''
-    main(50)
+
+    print(f'ВСЕГО найдено {count_articles} статей, содержащих ключевые слова {', '.join(KEYWORDS)}.')
